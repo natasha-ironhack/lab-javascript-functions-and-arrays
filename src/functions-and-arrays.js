c5aeb794d5ffc0168f17b07df6754fed56cc851a
Natasha Smith <pisceanchic@gmail.com>
2021-06-11 17:19:48 +0200

    Solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,13 +1,27 @@
 // Iteration #1: Find the maximum
 function maxOfTwoNumbers() {
-console.log(Math.max(1, 3));
-}
+  if (num1 > num2) return num1;
+  else return num2;
+  }
+//console.log(Math.max(1, 3));
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord(words) {}
+function findLongestWord(words) {
+  if (wordsParameter.length === 0){
+    return null
+  }
+  
+  let longest = " ";
+  
+  for (let word of wordsParameter){
+    if (word.length > longest.length) {
+      longest = word}
+  }
+  return longest
+}
   
 
 
@@ -15,13 +29,12 @@ function findLongestWord(words) {}
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
 function sumNumbers(numbers) {
-  // if (numbers.length === 0) return 0;
-  let temp = 0;
-  for (let number of numbers) {
-    temp += number;
-  }
+  if (numbers.length === 0) return 0;    
 
-  return temp;
+    for (let sum = 0, i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum;
 }
 
 
@@ -32,15 +45,34 @@ function sum() {}
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
-const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+const numbers = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+//function sum(numbers) {
+//  if (!numbers.length) return;
+   
+//    for (let sum = 0, i = 0; i < numbers.length; i++) {
+//      sum += numbers[i];
+//    }
+
+//return sum;
+//}
+  
+function averageNumbers(numbers) {
+    if (!numbers.length) return null;
+    
+else return sumNumbers(numbers) / numbers.length;
+}
 
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+function averageWordLength(wordsArr) { 
+  if (!wordsArr.length) return;
+
+  
+
+}
 
 // Bonus - Iteration #4.1
 function avg() {}
